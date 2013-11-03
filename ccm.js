66cb3919aefdcf77612530b047a62a6d97d348fe
Joakim Karlsson <joakim@jkarlsson.com>
2013-11-03 10:55:33 +0100

    Changed main module to expose a run method that takes an options arg
    This facilitates using ccm in, for instance, a grunt task as you're
    not required to use the internal modules of ccm directly if you don't
    want to.

--- ccm.js
+++ ccm.js
@@ -1,10 +1,42 @@
 var calculator = require('./lib/calculator');
 var results = require('./lib/results');
 var formatter = require('./lib/formatter');
+var fileCalculator = require('../lib/filecalculator');
+var fileFilter = require('../lib/fileFilter');
+var fs = require('fs');
+var util = require('util');
+var glob = require('glob');
+var minimatch = require('minimatch');
 
+function run(options) {
+  var filter = fileFilter.create(options.exclude);
+
+  var topResult = results.createResult(parseInt(options.results));
+
+  var ccmCalculator = {
+    calculate: calculator.calculate,
+  }
+
+  var calculator = fileCalculator.create(fs, ccmCalculator, topResult, function(file) {
+    return !filter(file);
+  });
+
+  glob(options.files, function(err, files) {
+    var res = calculator.calculateFor(files);
+    formatter.formatResult(res.results).forEach(function(res) {
+      console.log(res);
+    });
+
+    if(res.errors && res.errors.length > 0) {
+      console.log('\nFailed to parse the following files:');
+      res.errors.forEach(function(err) {
+        console.log(err.file + ' (' + err.line + ',' + err.col + '): ' + err.message);
+      });
+    }
+  });
+}
 
 module.exports = {
-  calculate: calculator.calculate,
-  createResult: results.createResult,
-  formatResult: formatter.formatResult
-}
+  run: run
+};
+
